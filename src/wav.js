e859a405320790021542a795f077d17e06f453dc
Jiayun Li <kenta@live.cn>
2017-06-24 17:08:04 +0800

    remove unnecessary console output

--- src/wav.js
+++ src/wav.js
@@ -2,7 +2,7 @@
  * @Author: Kitagawa.Kenta 
  * @Date: 2017-06-24 15:57:37 
  * @Last Modified by: Kitagawa.Kenta
- * @Last Modified time: 2017-06-24 16:57:35
+ * @Last Modified time: 2017-06-24 17:01:49
  */
 function WAV_fmt(data, dv, offset) {
     return {
@@ -20,7 +20,6 @@ function WAV_LIST(data, dv, offset, size) {
     let result = [];
     let listOffset = offset + 4;//(String)'info'
     while (listOffset < offset + size) {
-        console.log(listOffset, offset + 4 + size);
         let subchunk = {
             Name: int2Str(new Uint8Array(data, listOffset, 4, true)),
             Size: dv.getUint32(listOffset + 4, true),
@@ -43,7 +42,7 @@ function WAV_Decoder(result) {
 
     WavInfo.isRIFF = int2Str(new Uint8Array(data, 1 - 1, 4));
     WavInfo.Size = dv.getUint32(5 - 1, true);
-    WavInfo.isWAVE = int2Str(new Uint8Array(data, 9 - 1, 4));
+    WavInfo.Format = int2Str(new Uint8Array(data, 9 - 1, 4));
 
     let offset = 12;
     while (WavInfo.Size + 12 - offset >= 8) {
